8c1f5480d5920a3a036a9d885481765100ca9959
Este <stephan@vublty.com>
2026-03-03 12:04:20 +0000

    260303-1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 3000,
     host: '0.0.0.0',
     open: false,
     hmr: {
       clientPort: 3000
+    },
+    watch: {
+      usePolling: true, // Wichtig für Netzlaufwerke/Container
     }
   }
 })
