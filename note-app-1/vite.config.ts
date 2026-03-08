4d7b9946b57e0c8de0f307ba6af30f39e19b97fe
Bubba <gitlab_admin_5b3ad4@example.com>
2026-03-08 20:10:29 +0000

    260308-1

--- note-app-1/vite.config.ts
+++ note-app-1/vite.config.ts
@@ -1,18 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
   server: {
-    port: 3000,
     host: '0.0.0.0',
-    open: false,
-    hmr: {
-      clientPort: 3000
-    },
+    port: 5173,
     watch: {
-      usePolling: true, // Wichtig für Netzlaufwerke/Container
-    }
-  }
+      usePolling: true, // Wichtig für Docker auf Windows/macOS
+    },
+  },
 })
